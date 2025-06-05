e5013d46f09c9e35e9c56059e5c444dcf5423c2d
roman <rkuschev@icloud.com>
2025-06-05 20:21:35 +0300

    fix bugs

--- app/api/routers.py
+++ app/api/routers.py
@@ -25,7 +25,6 @@ async def read_warehouse_state(warehouse_id: str, product_id: str):
     logger = logging.getLogger(__name__)
     logger.info(f"Запрос состояния склада: warehouse_id={warehouse_id}, product_id={product_id}")
     
-    # Хардкодим значение для отладки
     return {
         "warehouse_id": warehouse_id,
         "product_id": product_id,
